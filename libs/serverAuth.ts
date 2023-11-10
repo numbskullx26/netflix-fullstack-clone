ab39083f252daef3b5d9a2528ebf29e855ac7674
Indrajeet Sarkar <sarkarraj1978@gmail.com>
2023-11-10 23:46:34 +0530

    debug : email fetching from useCurrentUser.ts

--- libs/serverAuth.ts
+++ libs/serverAuth.ts
@@ -1,12 +1,15 @@
-import { NextApiRequest } from "next";
+import { NextApiRequest, NextApiResponse } from "next";
 import { getSession } from "next-auth/react";
+// import { getServerSession } from "next-auth";
+
 import prismadb from "@/libs/prismadb";
+// import { authOptions } from "@/pages/api/auth/[...nextauth]";
 
 const serverAuth = async (req: NextApiRequest) => {
   const session = await getSession({ req });
 
   if (!session?.user?.email) {
-    throw new Error("Not Signed in");
+    throw new Error("Not signed in");
   }
 
   const currentUser = await prismadb.user.findUnique({
@@ -16,10 +19,36 @@ const serverAuth = async (req: NextApiRequest) => {
   });
 
   if (!currentUser) {
-    throw new Error("Not Signed in");
+    throw new Error("Not signed in");
   }
 
-  return currentUser;
+  return { currentUser };
 };
 
 export default serverAuth;
+
+// import { NextApiRequest } from "next";
+// import { getSession } from "next-auth/react";
+// import prismadb from "@/libs/prismadb";
+
+// const serverAuth = async (req: NextApiRequest) => {
+//   const session = await getSession({ req });
+
+//   if (!session?.user?.email) {
+//     throw new Error("Not Signed in");
+//   }
+
+//   const currentUser = await prismadb.user.findUnique({
+//     where: {
+//       email: session.user.email,
+//     },
+//   });
+
+//   if (!currentUser) {
+//     throw new Error("Not Signed in");
+//   }
+
+//   return currentUser;
+// };
+
+// export default serverAuth;
